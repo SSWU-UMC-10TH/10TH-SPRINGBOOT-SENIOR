722d1acb44058ba43bbcb8464598850eb353cc55
dyongini <abkimdoyo25@gmail.com>
2026-04-13 21:22:09 +0900

    [FEAT] Controller 생성

--- src/main/java/com/likelion/umc10th/domain/mission/controller/MissionController.java
+++ src/main/java/com/likelion/umc10th/domain/mission/controller/MissionController.java
@@ -1,4 +1,26 @@
 package com.likelion.umc10th.domain.mission.controller;
 
+import com.likelion.umc10th.domain.mission.dto.MissionReqDTO;
+import com.likelion.umc10th.domain.mission.dto.MissionResDTO;
+import com.likelion.umc10th.global.apiPayload.ApiResponse;
+import com.likelion.umc10th.global.apiPayload.code.GeneralSuccessCode;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class MissionController {
+
+    @PatchMapping("/missions/{userMissionId}")
+    public ApiResponse<MissionResDTO.MissionStatusResultDTO> completeMission(
+            @PathVariable(name = "userMissionId") Long userMissionId,
+            @RequestBody @Valid MissionReqDTO.MissionStatusDTO request
+            ) {
+
+        return ApiResponse.onSuccess(GeneralSuccessCode.OK, null);
+    }
 }
--- src/main/java/com/likelion/umc10th/domain/review/controller/ReviewController.java
+++ src/main/java/com/likelion/umc10th/domain/review/controller/ReviewController.java
@@ -1,4 +1,25 @@
 package com.likelion.umc10th.domain.review.controller;
 
+import com.likelion.umc10th.domain.review.dto.ReviewReqDTO;
+import com.likelion.umc10th.domain.review.dto.ReviewResDTO;
+import com.likelion.umc10th.global.apiPayload.ApiResponse;
+import com.likelion.umc10th.global.apiPayload.code.GeneralSuccessCode;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/reviews")
 public class ReviewController {
+
+    @PostMapping("/")
+    public ApiResponse<ReviewResDTO.CreateReviewResultDTO> createReview(
+            @RequestBody @Valid ReviewReqDTO.CreateReviewDTO request
+            ) {
+        return ApiResponse.onSuccess(GeneralSuccessCode.CREATED, null);
+    }
 }
