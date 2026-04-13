c87692509857a1cd7f623e35a35bec34229298d8
handagyeong <80438961+handagyeong@users.noreply.github.com>
2026-04-13 23:46:00 +0900

    feat: 5주차 워크북

--- src/main/java/com/example/umc10th/domain/member/controller/MemberController.java
+++ src/main/java/com/example/umc10th/domain/member/controller/MemberController.java
@@ -1,4 +1,30 @@
 package com.example.umc10th.domain.member.controller;
 
+import ch.qos.logback.core.model.processor.PhaseIndicator;
+import com.example.umc10th.domain.member.dto.MemberReqDto;
+import com.example.umc10th.domain.member.dto.MemberResDto;
+import com.example.umc10th.domain.member.exception.code.MemberSuccessCode;
+import com.example.umc10th.domain.member.service.MemberService;
+import com.example.umc10th.global.apiPayload.ApiResponse;
+import com.example.umc10th.global.apiPayload.code.BaseSuccessCode;
+import com.example.umc10th.global.apiPayload.code.GeneralSuccessCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping
 public class MemberController {
+
+    private final MemberService memberService;
+
+    //mypage
+    @PostMapping("/v1/users/me")
+    public ApiResponse<MemberResDto.GetInfo> getInfo(
+            @RequestBody MemberReqDto.GetInfo dto
+            ){
+        BaseSuccessCode code= MemberSuccessCode.OK;
+        return ApiResponse.onSuccess(code, memberService.getInfo(dto));
+    }
+
 }
--- src/main/java/com/example/umc10th/domain/member/exception/MemberException.java
+++ src/main/java/com/example/umc10th/domain/member/exception/MemberException.java
@@ -1,7 +1,9 @@
 package com.example.umc10th.domain.member.exception;
 
+import com.example.umc10th.global.apiPayload.code.BaseErrorCode;
+
 public class MemberException extends RuntimeException {
-    public MemberException(String message) {
-        super(message);
+    public MemberException(BaseErrorCode errorCode) {
+        super(errorCode);
     }
 }
