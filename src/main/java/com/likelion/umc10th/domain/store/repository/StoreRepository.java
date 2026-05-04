e743235fbeda4d46fcb6587c19c24765bc8fa553
dyongini <abkimdoyo25@gmail.com>
2026-05-04 21:15:46 +0900

    [FEAT] 스토어 관련 서비스

--- src/main/java/com/likelion/umc10th/domain/store/repository/StoreRepository.java
+++ src/main/java/com/likelion/umc10th/domain/store/repository/StoreRepository.java
@@ -1,4 +1,7 @@
 package com.likelion.umc10th.domain.store.repository;
 
-public interface StoreRepository {
+import com.likelion.umc10th.domain.store.entity.Store;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StoreRepository extends JpaRepository<Store, Integer> {
 }
