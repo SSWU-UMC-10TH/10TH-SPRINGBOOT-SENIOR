184b54de7d306960d9af5b7361dfa4cd082d2437
dyongini <abkimdoyo25@gmail.com>
2026-05-04 21:14:19 +0900

    [FEAT] 미션 서비스

--- src/main/java/com/likelion/umc10th/domain/mission/repository/MissionRepository.java
+++ src/main/java/com/likelion/umc10th/domain/mission/repository/MissionRepository.java
@@ -1,4 +1,33 @@
 package com.likelion.umc10th.domain.mission.repository;
 
-public interface MissionRepository {
+import com.likelion.umc10th.domain.mission.entity.Mission;
+import com.likelion.umc10th.domain.mission.entity.mapping.MemberMission;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface MissionRepository extends JpaRepository<Mission, Long> {
+
+    // 내가 진행중/완료한 미션 목록 조회
+    @Query("SELECT mm FROM MemberMission mm " +
+            "JOIN FETCH mm.mission m " +
+            "JOIN FETCH m.store s " +
+            "WHERE mm.member.id = :memberId AND mm.isCompleted = :isCompleted")
+    Page<MemberMission> findAllByMemberIdAndIsCompleted(
+            @Param("memberId") Long memberId,
+            @Param("isCompleted") Boolean isCompleted,
+            Pageable pageable);
+
+    // 현재 지역에서 도전 가능한 미션 목록 조회
+    @Query("SELECT m FROM Mission m " +
+            "JOIN FETCH m.store s " +
+            "JOIN FETCH s.region r " +
+            "WHERE r.id = :regionId AND m.deadline > CURRENT_TIMESTAMP")
+    Page<Mission> findAllByRegionId(@Param("regionId") Integer regionId, Pageable pageable);
+
+    // 완료한 미션 갯수 카운트
+    @Query("SELECT COUNT(mm) FROM MemberMission mm WHERE mm.member.id = :memberId AND mm.isCompleted = true")
+    Long countCompletedMissions(@Param("memberId") Long memberId);
 }
--- src/main/java/com/likelion/umc10th/domain/mission/service/MissionService.java
+++ src/main/java/com/likelion/umc10th/domain/mission/service/MissionService.java
@@ -1,4 +1,48 @@
 package com.likelion.umc10th.domain.mission.service;
 
+import com.likelion.umc10th.domain.mission.dto.MissionResDTO;
+import com.likelion.umc10th.domain.mission.entity.mapping.MemberMission;
+import com.likelion.umc10th.domain.mission.repository.MissionRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class MissionService {
+
+    private final MissionRepository missionRepository;
+
+    public MissionResDTO.MyMissionListDTO getMyMissionList(Long memberId, Boolean isCompleted, Integer page) {
+
+        Pageable pageable = PageRequest.of(page > 0 ? page - 1 : 0, 10, Sort.by("createdAt").descending());
+        Page<MemberMission> mmPage = missionRepository.findAllByMemberIdAndIsCompleted(memberId, isCompleted, pageable);
+
+        List<MissionResDTO.MyMissionDetailDTO> missionDetailDTOList = mmPage.getContent().stream()
+                .map(mm -> MissionResDTO.MyMissionDetailDTO.builder()
+                        .userMissionId(mm.getId())
+                        .point(mm.getMission().getPoint().intValue())
+                        .storeName(mm.getMission().getStore().getName())
+                        .status(mm.getIsCompleted() ? "COMPLETED" : "CHALLENGING")
+                        .condition(mm.getMission().getCondition())
+                        .createdAt(mm.getCreatedAt().toLocalDate())
+                        .build()
+                ).toList();
+
+        return MissionResDTO.MyMissionListDTO.builder()
+                .missionList(missionDetailDTOList)
+                .totalPages(mmPage.getTotalPages())
+                .totalElements(mmPage.getTotalElements())
+                .isLast(mmPage.isLast())
+                .build();
+    }
+
+
 }
