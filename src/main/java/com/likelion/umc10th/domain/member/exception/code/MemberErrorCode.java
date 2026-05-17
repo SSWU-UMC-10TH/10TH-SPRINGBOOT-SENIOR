0babbd225b4051515d9485c9634bb83464f7633f
dyongini <abkimdoyo25@gmail.com>
2026-05-18 01:02:36 +0900

    [Feat] 회원가입

--- src/main/java/com/likelion/umc10th/domain/member/exception/code/MemberErrorCode.java
+++ src/main/java/com/likelion/umc10th/domain/member/exception/code/MemberErrorCode.java
@@ -1,4 +1,24 @@
 package com.likelion.umc10th.domain.member.exception.code;
 
-public enum MemberErrorCode {
+import com.likelion.umc10th.global.apiPayload.code.BaseErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum MemberErrorCode implements BaseErrorCode {
+
+    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND,
+            "MEMBER404_1",
+            "존재하지 않는 멤버입니다."),
+
+
+    DUPLICATE_USERNAME(HttpStatus.CONFLICT,
+            "MEMBER409_1",
+            "이미 존재하는 아이디 입니다.");
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
 }
