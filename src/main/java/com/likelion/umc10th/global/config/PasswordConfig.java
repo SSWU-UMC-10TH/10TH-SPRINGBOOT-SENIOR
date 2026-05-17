a53fb9a5090e03169cae733799bb87541a5d2320
dyongini <abkimdoyo25@gmail.com>
2026-05-18 01:03:37 +0900

    [Feat] 인증 인가 관련, public/private api 설정

---
+++ src/main/java/com/likelion/umc10th/global/config/PasswordConfig.java
@@ -0,0 +1,14 @@
+package com.likelion.umc10th.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class PasswordConfig {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
